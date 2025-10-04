5e89cf206877c114c4caa5b0f2675430a7e6959e
Eddy Llumiquinga <81939375+Eddyll1@users.noreply.github.com>
2025-10-04 18:30:59 -0500

    Add files via upload

---
+++ src/main/java/ejercicio1/coderE/password_strength/PasswordStrengthApplication.java
@@ -0,0 +1,13 @@
+package ejercicio1.coderE.password_strength;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordStrengthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordStrengthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ejercicio1/coderE/password_strength/PasswordStrengthApplicationTests.java
@@ -0,0 +1,13 @@
+package ejercicio1.coderE.password_strength;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordStrengthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
